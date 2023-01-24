57a92aeab311a8abb13c50acdc38a20931693866
Preston Stewart <123pstewart@frhsd.com>
2023-01-24 10:52:38 -0500

    Revisions 1

--- js/gallery.js
+++ js/gallery.js
@@ -33,18 +33,46 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
+	if(mCurrentIndex >= mImages.length)
 	{
-		document.slide3.src = mImages[0];
-		if(k < path3.length - 1){ k++;} else {k = 0;}
-		setTimeout("swapPhoto()",2000);
-	 }
-	console.log('swap photo');
+		mCurrentIndex = 0;
+	}
+	if(mCurrentIndex < 0)
+	{
+		mCurrentIndex = mImages.length-1;
+	}
+	
+		document.getElementById('photo').src = mImages[mCurrentIndex].img;
+		var loc = document.getElementsByClassName('location');
+		loc[0].innerHTML = "Location: " + mImages[mCurrentIndex].location;
+		var des = document.getElementsByClassName('description');
+		des[0].innerHTML = "Description: " + mImages[mCurrentIndex].description;
+		var dt = document.getElementsByClassName('date');
+		dt[0].innerHTML = "Date: " + mImages[mCurrentIndex].date;
+
+		mLastFrameTime = 0;
+		mCurrentIndex += 1;
 }
 
-var mImages = []
-mImages[0]= "http://placehold.it/518x389?text=index+0";
-mImages[1]= "http://placehold.it/518x389?text=index+1";
-mImages[2]= "http://placehold.it/518x389?text=index+2";
+function toggleDetails()
+	{
+		if($(".moreIndicator").hasClass("rot90"))
+		{
+			$( ".moreIndicator" ).removeClass("rot90");
+			$(".moreIndicator").addClass("rot270");
+		}
+		else {
+			$( ".moreIndicator" ).removeClass("rot270");
+			$(".moreIndicator").addClass("rot90");
+		}
+		$( ".details").slideToggle( "slow", "linear");
+	}
+
+
+var mImages = [];
+// mImages[0]= "http://placehold.it/518x389?text=index+0";
+// mImages[1]= "http://placehold.it/518x389?text=index+1";
+// mImages[2]= "http://placehold.it/518x389?text=index+2";
 
 // Counter for the mImages array
 var mCurrentIndex = 0;
@@ -59,10 +87,10 @@ for (const { date, description } of mImages) {
   // If the output object doesn't have a property
   // key that matches the current object's name
   // create it by assigning it zero
-  output[] ??= 0;
+ // output[] ??= 0;
 
   // Then increase its amount
-  output[] += amount;
+  //output[] += amount;
 }
 
 console.log(output);
@@ -70,13 +98,7 @@ console.log(output);
 // // XMLHttpRequest variable
 // var mRequest = new XMLHttpRequest();
 
-// mRequest.onreadystatechange= function() {
-// 	  if (this.readyState == 4 && this.status == 200) {
-// 	   mJson= JSON.parse(mRequest.responseText)
-// 	  }
-// 	};
-// 	mRequest.open("GET", mUrl, true);
-// 	mRequest.send();
+
 
 var mCurrentIndex = 0;
 // Array holding GalleryImage objects (see below).
@@ -85,15 +107,19 @@ var mRequest = new XMLHttpRequest();
 // Holds the retrived JSON information
 var mJson;
 
+var mUrl;
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-mRequest.onreadystatechange = function (){
-	if(this.ready == 4 && this.status ==200){
-		mJson = Json.parse(mRequest.responseText);
-		console.log(mJson);
-	}
+function fetchJSON()
+{
+	mRequest.onreadystatechange= function() {
+		if (this.readyState == 4 && this.status == 200) {
+		 mJson= JSON.parse(mRequest.responseText)
+		}
+	  };
+	  mRequest.open("GET", mUrl, true);
+	  mRequest.send();
 }
-
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
@@ -104,12 +130,39 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 	}
 }
 
+
+function iterateJSON(mJson)
+{
+	for( x = 0; x < mJson.images.length; x++)
+	{
+		mImages[x] = new GalleryImage();
+		mImages[x].location = mJson.images[x].imgLocation;
+		mImages[x].description = mJson.images[x].description;
+		mImages[x].date = mJson.images[x].date;
+		mImages[x].img = mJson.images[x].imgPath;
+	}
+}
 $(document).ready( function() {
-	
-	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
+	$('#nextPhoto').position({
+		my: "right bottom",
+		at: "right bottom",
+		of: "#nav",
+	});
 	
 });
+
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  for (const [key, value] of urlParams) {
+	console.log(`${key}:${value}`);
+	mUrl = value;
+  }
+  if(mUrl == undefined)
+(
+	mUrl = 'images.json'
+)
+fetchJSON();
 
 window.addEventListener('load', function() {
 	
@@ -118,10 +171,10 @@ window.addEventListener('load', function() {
 }, false);
 
 function GalleryImage() {
-	let location= ""
-	let description= ""
-	let date= ""
-	let img= ""
+	var location;
+	var description;
+	var date;
+	var img;
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo
